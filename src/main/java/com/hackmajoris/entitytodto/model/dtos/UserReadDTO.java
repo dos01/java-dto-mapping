4f201bce981ce291481971ffd611b935552c177a
Doolitha Samaranayake <doolithasamaranayaka@gmail.com>
2021-10-16 10:54:02 +0530

    Using lombark for getters and setters

--- src/main/java/com/hackmajoris/entitytodto/model/dtos/UserReadDTO.java
+++ src/main/java/com/hackmajoris/entitytodto/model/dtos/UserReadDTO.java
@@ -1,25 +1,13 @@
 package com.hackmajoris.entitytodto.model.dtos;
 
+import lombok.Data;
+
+@Data
 public class UserReadDTO implements DTOEntity {
 
-    private String name;
-    private String email;
+	private String name;
+	private String email;
 
-    public UserReadDTO(){}
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public UserReadDTO() {
+	}
 }
--- src/main/java/com/hackmajoris/entitytodto/model/dtos/UserUpdateDTO.java
+++ src/main/java/com/hackmajoris/entitytodto/model/dtos/UserUpdateDTO.java
@@ -1,43 +1,20 @@
 package com.hackmajoris.entitytodto.model.dtos;
 
+import lombok.Data;
+
+@Data
 public class UserUpdateDTO implements DTOEntity {
 
-    private String name;
-    private String email;
-    private String password;
+	private String name;
+	private String email;
+	private String password;
 
-    public UserUpdateDTO(){}
+	public UserUpdateDTO() {
+	}
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "UserUpdateDTO{" +
-                "name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "UserUpdateDTO{" + "name='" + name + '\'' + ", email='" + email + '\'' + ", password='" + password + '\''
+				+ '}';
+	}
 }
